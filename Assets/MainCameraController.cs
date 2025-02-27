64455277ce6954eab93579e4c248007205cad764
nathan <nathwood@udel.edu>
2025-02-27 17:45:41 -0500

    Message

---
+++ Assets/MainCameraController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MainCameraController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("computer science more like boring");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
